f34af49c660f2013c1869b1f16b6bedfcb4ab0b4
yashsharma0558 <yashsharma0558@gmail.com>
2023-12-10 16:19:33 +0530

    ->added feature to display the image of the current alphabet when drawing

--- app/src/main/java/com/example/inkAlankar/GameFragment.kt
+++ app/src/main/java/com/example/inkAlankar/GameFragment.kt
@@ -1,32 +1,18 @@
 package com.example.inkAlankar
 
-import android.app.AlertDialog
-import android.content.ContentValues
-import android.content.Context
-import android.content.DialogInterface
+import android.R
 import android.graphics.Bitmap
-import android.net.Uri
-import android.os.AsyncTask
 import android.os.Bundle
-import android.os.Environment
-import android.provider.ContactsContract.Data
-import android.provider.MediaStore
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.Window
 import android.view.WindowManager
 import android.widget.Toast
-import com.example.inkAlankar.DataSource
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import com.example.inkAlankar.databinding.FragmentGameBinding
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.Firebase
 import com.google.firebase.database.database
-import java.io.OutputStream
 
 
 class GameFragment : Fragment() {
@@ -34,6 +20,88 @@ class GameFragment : Fragment() {
     private var _binding: FragmentGameBinding? = null
     private val binding get() = _binding!!
     private var bitmapList = mutableListOf<Bitmap>()
+    private val imageList = mutableListOf(
+        com.example.inkAlankar.R.drawable.draw_1,
+        com.example.inkAlankar.R.drawable.draw_2,
+        com.example.inkAlankar.R.drawable.draw_3,
+        com.example.inkAlankar.R.drawable.draw_4,
+        com.example.inkAlankar.R.drawable.draw_5,
+        com.example.inkAlankar.R.drawable.draw_6,
+        com.example.inkAlankar.R.drawable.draw_7,
+        com.example.inkAlankar.R.drawable.draw_8,
+        com.example.inkAlankar.R.drawable.draw_9,
+        com.example.inkAlankar.R.drawable.draw_10,
+        com.example.inkAlankar.R.drawable.draw_11,
+        com.example.inkAlankar.R.drawable.draw_12,
+        com.example.inkAlankar.R.drawable.draw_13,
+        com.example.inkAlankar.R.drawable.draw_14,
+        com.example.inkAlankar.R.drawable.draw_15,
+        com.example.inkAlankar.R.drawable.draw_16,
+        com.example.inkAlankar.R.drawable.draw_17,
+        com.example.inkAlankar.R.drawable.draw_18,
+        com.example.inkAlankar.R.drawable.draw_19,
+        com.example.inkAlankar.R.drawable.draw_20,
+        com.example.inkAlankar.R.drawable.draw_21,
+        com.example.inkAlankar.R.drawable.draw_22,
+        com.example.inkAlankar.R.drawable.draw_23,
+        com.example.inkAlankar.R.drawable.draw_24,
+        com.example.inkAlankar.R.drawable.draw_25,
+        com.example.inkAlankar.R.drawable.draw_26,
+        com.example.inkAlankar.R.drawable.draw_27,
+        com.example.inkAlankar.R.drawable.draw_28,
+        com.example.inkAlankar.R.drawable.draw_29,
+        com.example.inkAlankar.R.drawable.draw_30,
+        com.example.inkAlankar.R.drawable.draw_31,
+        com.example.inkAlankar.R.drawable.draw_32,
+        com.example.inkAlankar.R.drawable.draw_33,
+        com.example.inkAlankar.R.drawable.draw_34,
+        com.example.inkAlankar.R.drawable.draw_35,
+        com.example.inkAlankar.R.drawable.draw_36,
+        com.example.inkAlankar.R.drawable.draw_37,
+        com.example.inkAlankar.R.drawable.draw_38,
+        com.example.inkAlankar.R.drawable.draw_39,
+        com.example.inkAlankar.R.drawable.draw_40,
+        com.example.inkAlankar.R.drawable.draw_41,
+        com.example.inkAlankar.R.drawable.draw_42,
+        com.example.inkAlankar.R.drawable.draw_43,
+        com.example.inkAlankar.R.drawable.draw_44,
+        com.example.inkAlankar.R.drawable.draw_45,
+        com.example.inkAlankar.R.drawable.draw_46,
+        com.example.inkAlankar.R.drawable.draw_47,
+        com.example.inkAlankar.R.drawable.draw_48,
+        com.example.inkAlankar.R.drawable.draw_49,
+        com.example.inkAlankar.R.drawable.draw_50,
+        com.example.inkAlankar.R.drawable.draw_51,
+        com.example.inkAlankar.R.drawable.draw_52,
+        com.example.inkAlankar.R.drawable.draw_53,
+        com.example.inkAlankar.R.drawable.draw_54,
+        com.example.inkAlankar.R.drawable.draw_55,
+        com.example.inkAlankar.R.drawable.draw_56,
+        com.example.inkAlankar.R.drawable.draw_57,
+        com.example.inkAlankar.R.drawable.draw_58,
+        com.example.inkAlankar.R.drawable.draw_59,
+        com.example.inkAlankar.R.drawable.draw_60,
+        com.example.inkAlankar.R.drawable.draw_61,
+        com.example.inkAlankar.R.drawable.draw_62,
+        com.example.inkAlankar.R.drawable.draw_63,
+        com.example.inkAlankar.R.drawable.draw_64,
+        com.example.inkAlankar.R.drawable.draw_65,
+        com.example.inkAlankar.R.drawable.draw_66,
+        com.example.inkAlankar.R.drawable.draw_67,
+        com.example.inkAlankar.R.drawable.draw_68,
+        com.example.inkAlankar.R.drawable.draw_69,
+        com.example.inkAlankar.R.drawable.draw_70,
+        com.example.inkAlankar.R.drawable.draw_71,
+        com.example.inkAlankar.R.drawable.draw_72,
+        com.example.inkAlankar.R.drawable.draw_73,
+        com.example.inkAlankar.R.drawable.draw_74,
+        com.example.inkAlankar.R.drawable.draw_75,
+        com.example.inkAlankar.R.drawable.draw_76,
+        com.example.inkAlankar.R.drawable.draw_77,
+        com.example.inkAlankar.R.drawable.draw_78,
+        com.example.inkAlankar.R.drawable.draw_79,
+        com.example.inkAlankar.R.drawable.background
+    )
     private val valuesList = mutableListOf(
         "क (ka)",
         "ख (kha)",
@@ -118,6 +186,7 @@ class GameFragment : Fragment() {
     )
     private var counter: Int = 0
     private lateinit var path: String
+    private lateinit var list: IntArray
 
     override fun onCreate(savedInstanceState: Bundle?) {
         arguments?.let {
@@ -137,12 +206,17 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
+
+//        for (i in 0 until list.length()) {
+//            val id = list.getResourceId(i, -1)
+//        }
         //reset canvas
         binding.button4.setOnClickListener {
             resetCanvas()
         }
         //change button text to valueList's first object
         binding.textView13.text = valuesList[counter]
+        binding.imageView.setImageResource(imageList[counter])
 
         //save bitmap in list
         binding.button3.setOnClickListener {
@@ -185,12 +259,15 @@ class GameFragment : Fragment() {
 
 
     }
+
     private fun showOverlay() {
         binding.progressBar.visibility = View.VISIBLE
         binding.overlayBackground.visibility = View.VISIBLE
         // Disable user interactions if needed
-        activity?.window?.setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
-            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+        activity?.window?.setFlags(
+            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+        )
     }
 
     private fun hideOverlay() {
@@ -203,12 +280,14 @@ class GameFragment : Fragment() {
     }
 
     private fun updateWord() {
+
         counter++
         if (counter < valuesList.size) {
             binding.textView13.text = valuesList[counter]
+            binding.imageView.setImageResource(imageList[counter])
         }
         if (counter == COUNT - 1) {
-            binding.button3.text = getString(R.string.submit)
+            binding.button3.text = getString(com.example.inkAlankar.R.string.submit)
 
         }
     }
